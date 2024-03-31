e2aad7b57037486e1e3718a93992ea21abe5d1dd
Quan Nguyen <quanjenkey@gmail.com>
2024-03-31 21:24:20 +0700

    Delete build/lib directory

--- build/lib/bunny/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .language_model.bunny_phi import BunnyPhiForCausalLM, BunnyPhiConfig
-from .language_model.bunny_stablelm import BunnyStableLMForCausalLM, BunnyStableLMConfig
-from .language_model.bunny_qwen import BunnyQwenForCausalLM, BunnyQwenConfig
