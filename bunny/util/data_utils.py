1059bb25bf8aa30b28b79a5a966a33c9500cb50a
Quan Nguyen <quanjenkey@gmail.com>
2024-04-04 13:34:19 +0700

    Update data_utils.py

--- bunny/util/data_utils.py
+++ bunny/util/data_utils.py
@@ -7,16 +7,17 @@ from typing import Dict, Sequence, Optional
 import torch
 
 import transformers
-
+from packaging import version
 from bunny.constants import IGNORE_INDEX, DEFAULT_IMAGE_TOKEN
 from torch.utils.data import Dataset
-
+import tokenizers
 from bunny import conversation as conversation_lib
 
 from bunny.util.mm_utils import tokenizer_image_token
 
 from PIL import Image
 
+IS_TOKENIZER_GREATER_THAN_0_14 = version.parse(tokenizers.__version__) >= version.parse('0.14')
 
 @dataclass
 class DataArguments:
@@ -48,6 +49,96 @@ def preprocess_multimodal(
 
     return sources
 
+def preprocess_mpt(
+    sources,
+    tokenizer: transformers.PreTrainedTokenizer,
+    has_image: bool = False
+) -> Dict:
+    conv = conversation_lib.default_conversation.copy()
+    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+
+    # Apply prompt templates
+    conversations = []
+    for i, source in enumerate(sources):
+        if roles[source[0]["from"]] != conv.roles[0]:
+            # Skip the first one if it is not from human
+            source = source[1:]
+
+        conv.messages = []
+        for j, sentence in enumerate(source):
+            role = roles[sentence["from"]]
+            assert role == conv.roles[j % 2], f"{i}"
+            conv.append_message(role, sentence["value"])
+        conversations.append(conv.get_prompt())
+    # Tokenize conversations
+    # print(conversations)
+    if has_image:
+        input_ids = torch.stack([tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+    else:
+        input_ids = tokenizer(
+            conversations,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+        ).input_ids
+
+    targets = input_ids.clone()
+    assert conv.sep_style == conversation_lib.SeparatorStyle.MPT
+
+    # Mask targets
+    sep = conv.sep + conv.roles[1]
+    for conversation, target in zip(conversations, targets):
+        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+
+        rounds = conversation.split(conv.sep)
+        re_rounds = [conv.sep.join(rounds[:3])] # system + user + gpt
+        for conv_idx in range(3, len(rounds), 2):
+            re_rounds.append(conv.sep.join(rounds[conv_idx:conv_idx+2]))    # user + gpt
+        cur_len = 0
+        target[:cur_len] = IGNORE_INDEX
+        # print(re_rounds)
+        for i, rou in enumerate(re_rounds):
+            cur_len = cur_len + i
+            if rou == "":
+                break
+
+            parts = rou.split(sep)
+            if len(parts) != 2:
+                break
+            parts[0] += sep
+            # print(parts[0])
+
+            if has_image:
+                round_len = len(tokenizer_image_token(rou, tokenizer))
+                instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 1
+            else:
+                round_len = len(tokenizer(rou).input_ids)
+                instruction_len = len(tokenizer(parts[0]).input_ids)
+
+            if i != 0 and getattr(tokenizer, 'legacy', False) and IS_TOKENIZER_GREATER_THAN_0_14:
+                round_len += 1
+                instruction_len += 1
+
+            target[cur_len : cur_len + instruction_len] = IGNORE_INDEX
+            cur_len += round_len
+        target[cur_len:] = IGNORE_INDEX
+        # print(cur_len)
+        # print(input_ids)
+        # print(targets)
+
+        # if cur_len < tokenizer.model_max_length:
+        #     if cur_len != total_len:
+        #         target[:] = IGNORE_INDEX
+        #         print(
+        #             f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+        #             f" (ignored)"
+        #         )
+    return dict(
+        input_ids=input_ids,
+        labels=targets,
+    )
+
 
 def preprocess_bunny(
         sources,
@@ -70,6 +161,7 @@ def preprocess_bunny(
             assert role == conv.roles[j % 2], f"{i}"
             conv.append_message(role, sentence["value"])
         conversations.append(conv.get_prompt())
+
 
     # Tokenize conversations
 
@@ -130,7 +222,8 @@ def preprocess_bunny(
                     f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
                     f" (ignored)"
                 )
-
+    # print(input_ids)
+    # print(targets)
     return dict(
         input_ids=input_ids,
         labels=targets,
@@ -155,6 +248,9 @@ def preprocess_plain(
     for target, source in zip(targets, sources):
         tokenized_len = len(tokenizer_image_token(source[0]['value'], tokenizer))
         target[:tokenized_len] = IGNORE_INDEX
+    
+    # print(input_ids)
+    # print(targets)
 
     return dict(input_ids=input_ids, labels=targets)
 
@@ -169,6 +265,9 @@ def preprocess(
 
     if conversation_lib.default_conversation.version == "bunny":
         return preprocess_bunny(sources, tokenizer, has_image=has_image)
+    
+    if conversation_lib.default_conversation.version == "mpt":
+        return preprocess_mpt(sources, tokenizer, has_image=has_image)
 
 
 class LazySupervisedDataset(Dataset):
